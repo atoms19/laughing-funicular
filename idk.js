06405decb98320fab853bb077ad8e659626583ea
atoms19 <74504022+atoms19@users.noreply.github.com>
2020-11-30 08:48:14 +0530

    Create idk.js

---
+++ idk.js
@@ -0,0 +1,81 @@
+//-----finder object------------
+
+
+class finder{
+    constructor(el){
+          this.elem=document.querySelector(el)     
+    }
+hide(){
+    this.elem.style.display="none"
+}
+show(){
+    this.elem.style.display="block"
+}
+txt(text){
+    this.elem.textContent=text
+}
+html(code){
+    this.elem.innerHTML=code
+}
+style(prp,val=""){
+if(val==""){
+   return window.getComputedStyle(elem, null).getPropertyValue(prp); 
+}
+ this.elem.style[prp]=val
+}
+addClass(cls){
+    this.elem.classList.add(cls)
+}
+removeClass(cls){
+this.elem.classList.remove(cls)
+}
+val(t=""){
+    
+    
+    if(t==""){
+    return this.elem.value
+    }else{
+        this.elem.value=t
+    }}
+add(nod){
+   this.elem.appendChild(nod) 
+}        
+child(){
+    return this.elem
+}
+attr(at,vl=""){
+    if(vl==""){
+        this.elem.getAttribute(at)
+    }else{
+        this.elem.setAttribute(at,vl)
+   }   
+}
+
+    
+}
+
+
+//----------functions-----------
+
+//timer
+function timer(cd,ti=1000){
+    this.code="()=>{"+cd+"}"
+    setTimeout(eval(this.code),ti)
+}
+//making a class
+function makeClass(name,stuf){
+    this.classic=document.createElement("style")
+   this.stuf="."+name+"{"+stuf+"}"
+    this.classic.innerHTML=this.stuf
+    
+    document.body.appendChild(this.classic)
+}
+//making and appending
+function create(eli,id){
+    this.element=document.createElement(eli)
+ 
+this.element.setAttribute("id",id)   
+    document.body.appendChild(this.element)
+    return new finder("#"+id)
+}
+
